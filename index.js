2fe869632b14bddddfa5ade452b390af6a8f842a
Imtiaz-Zihad <mdimtiazzihad@gmail.com>
2025-01-05 19:25:50 +0600

    Add JWT authentication and user role verification

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 
@@ -33,13 +34,62 @@ async function run() {
     const reviewCollection = client.db("bistroDb").collection("reviews");
     const cartCollection = client.db("bistroDb").collection("carts");
 
-    // User Collection
+    //jwt token api
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "5h",
+      });
+      res.send({ token });
+    });
 
-    app.get("/users", async (req, res) => {
+    //verify token
+    const verifyToken = (req, res, next) => {
+      if (!req.headers.authorization) {
+        return res.status(401).send({ message: "Unauthorized Request" });
+      }
+      const token = req.headers.authorization.split(" ")[1];
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+          return res.status(401).send({ message: "Unauthorized Request" });
+        }
+        req.decoded = decoded;
+        next();
+      });
+    };
+
+    // verify admin role api 
+    const verifyAdmin = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      const isAdmin = user?.role === "admin";
+      if (!isAdmin) {
+        return res.status(403).send({ message: "Forbidden Request" });
+      }next();
+    };
+    // User Collection
+    app.get("/users", verifyToken,verifyAdmin, async (req, res) => {
       const result = await usersCollection.find().toArray();
       res.send(result);
     });
-    app.post("/users", async (req, res) => {
+
+    app.get("users/admin/:id", verifyToken, async (req, res) => {
+      const id = req.params.id;
+      if (email !== req.decoded.email) {
+        return res.status(403).send({ message: "Unauthorized Request" });
+      }
+
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      let admin = false;
+      if (user) {
+        admin = user?.role === "admin";
+      }
+      res.send({ admin });
+    });
+
+    app.post("/users", verifyToken, async (req, res) => {
       const user = req.body;
       // insert email if user does not exist
       const query = { email: user.email };
@@ -52,7 +102,7 @@ async function run() {
     });
 
     //change user role
-    app.patch("/users/admin/:id", async (req, res) => {
+    app.patch("/users/admin/:id",verifyToken,verifyAdmin, async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const update = { $set: { role: "admin" } };
@@ -61,7 +111,7 @@ async function run() {
     });
 
     //delete user
-    app.delete("/users/:id", async (req, res) => {
+    app.delete("/users/:id",verifyToken,verifyAdmin, async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await usersCollection.deleteOne(query);
